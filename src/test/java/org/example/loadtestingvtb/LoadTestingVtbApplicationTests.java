5cf0f88fd2a8f93f6d5e8e4cdd85223b5c75e24a
Vlada Vasileva <vvavlada@gmail.com>
2026-02-11 15:09:52 +0300

    Сделала папку с тестами видимой

---
+++ src/test/java/org/example/loadtestingvtb/LoadTestingVtbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.loadtestingvtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadTestingVtbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
